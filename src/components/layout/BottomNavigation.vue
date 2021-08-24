5df697fbf250dcce84264df23e3439ebe83649df
weewwe26 <78019239+weewwe26@users.noreply.github.com>
2021-08-24 13:52:33 +0900

    src 내용 수정
    home.vue 지금까지 한 부분 수정했습니다.

--- src/components/layout/BottomNavigation.vue
+++ src/components/layout/BottomNavigation.vue
@@ -1,84 +1,62 @@
 <template>
-    <nav class="nav">
-        <a href="#/" class="nav__link">
-            <div>
-                <b-icon icon="house-fill" class="nav__icons"></b-icon>
-            </div>
-            <span class="nav__text">홈</span>
-        </a>
-
-        <a href="#/sms" class="nav__link">
-            <div>
-                <b-icon icon="chat-left-dots-fill" class="nav__icons"></b-icon>
-            </div>
-            <span class="nav__text">문자열람</span>
-        </a>
-
-        <a href="#slide-3" class="nav__link">
-            <div>
-                <b-icon icon="graph-up" class="nav__icons"></b-icon>
-            </div>
-            <span class="nav__text">상세분석</span>
-        </a>
-
-        <a href="#slide-4" class="nav__link">
-            <div>
-                <b-icon icon="lightning-fill" class="nav__icons"></b-icon>
-            </div>
-            <span class="nav__text">예방팁</span></a
-        >
-
-        <a href="#slide-5" class="nav__link">
-            <div>
-                <b-icon icon="list" class="nav__icons"></b-icon>
-            </div>
-            <span class="nav__text">메뉴</span></a
-        >
-    </nav>
+  <nav class="nav">
+    <div class="nav__link" v-for="(item, i) in links" :key="item">
+      <a :href="links[i]" class="nav__link">
+        <b-icon :icon="icons[i]" class="nav__icons"/>
+        <span>{{menus[i]}}</span>
+      </a>
+    </div>
+  </nav>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: ['#/', '#/about', '#/detail', '#/setting', '#/menus'],
+      icons: ['house-fill', 'chat-left-dots-fill', 'graph-up', 'lightning-fill', 'list'],
+      menus: ['홈', '문자열람', '상세분석', '예방팁', '메뉴'],
+    }
+  }
+}
 </script>
-
 <style>
 .nav {
-    /* position: fixed; */
-    bottom: 0;
-    width: 100%;
-    height: 55px;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
-    background-color: #ffffff;
-    display: flex;
-    overflow-x: auto;
+  text-decoration: none;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 55px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+  background-color: #ffffff;
+  display: flex;
+  overflow-x: auto;
 }
 
 .nav__link {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    flex-grow: 1;
-    min-width: 50px;
-    overflow: hidden;
-    white-space: nowrap;
-    font-family: sans-serif;
-    font-size: 13px;
-    color: #444444;
-    text-decoration: none;
-    -webkit-tap-highlight-color: transparent;
-    transition: background-color 0.1s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  min-width: 50px;
+  overflow: hidden;
+  white-space: nowrap;
+  font-family: sans-serif;
+  font-size: 13px;
+  color: #444444;
+  -webkit-tap-highlight-color: transparent;
+  transition: background-color 0.1s ease-in-out;
 }
 
 .nav__link:hover {
-    background-color: #eeeeee;
+  background-color: #eeeeee;
 }
 
 .nav__link--active {
-    color: #009578;
+  color: #009578;
 }
 
 .nav__icon {
-    font-size: 18px;
+  font-size: 18px;
 }
 </style>
